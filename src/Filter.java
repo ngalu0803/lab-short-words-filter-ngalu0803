e583da1ca9dbad11dd861ef7e800c72fc719baae
ngalu0803 <55891417+ngalu0803@users.noreply.github.com>
2020-04-27 19:21:00 -0400

    Update Filter.java

--- src/Filter.java
+++ src/Filter.java
@@ -3,21 +3,22 @@ import java.util.ArrayList;
 public interface Filter {
     static ArrayList<Object> applyFilter(ArrayList<Object> objects, Filter f) {
         ArrayList<Object> filtered = new ArrayList<>();
-        boolean accept(Object x);
-        static ArrayList<Object> applyFilter(ArrayList<Object> objects, Filter f) {
-            //creating an array list to store the result
-            ArrayList<Object> result = new ArrayList<Object>();
-            //looping through each object
-            for (Object ob : objects) {
-                //if f accepts ob, adding ob to result
-                if (f.accept(ob)) {
-                    result.add(ob);
-                }
-            }
-            //returning result
-            return result;
-        }
-    }
+    
+
+boolean accept(Object x);
+
+static ArrayList<Object> applyFilter(ArrayList<Object> objects, Filter f) {
+ArrayList<Object> filtered = new ArrayList<>();
+
+ArrayList<Object> result = new ArrayList<Object>();
+
+for (Object ob : objects) {
+if (f.accept(ob)) {
+result.add(ob);
+
+}
+return result;
+}
         
         
         
@@ -25,7 +26,6 @@ public interface Filter {
         
         
         
-        
-        return filtered;
-    }
+       
+    
 }
